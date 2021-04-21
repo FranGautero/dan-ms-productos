5511548894651d2402e9228138af8fddfaf51b69
Francisco Gautero <francisco.gautero@gmail.com>
2021-04-20 21:42:14 -0300

    Initial commit

---
+++ src/test/java/com/labs/danmsproductos/DanMsProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labs.danmsproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
